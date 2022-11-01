5dd07dcab2af48a069b7e050a705dea0309f17de
rahul01r <rrdubeynibm@gmail.com>
2022-11-01 15:33:09 +0530

    [rahul] Add:UC1-CartesianSystem

---
+++ LineComprision/Comprasion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComprision
+{
+    public class Comprasion
+    {
+        int x1, x2, y1, y2;
+        public Comprasion(int _x1, int _x2, int _y1, int _y2)
+        {
+            this.x1 = _x1;
+            this.x2 = _x2;
+            this.y1 = _y1;
+            this.y2 = _y2;
+        }
+        public void CalculateLength()
+        {
+            double lineLength = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine(lineLength);
+        }
+    }
+}
---
+++ LineComprision/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LineComprision
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Comprasion comprasion = new Comprasion(1, 2, 3, 4);
+            comprasion.CalculateLength();
+        }
+    }
+}
