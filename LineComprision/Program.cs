1edfbf2b038efb0c58ea5fc3f1cf665b33d17b0e
rahul01r <rrdubeynibm@gmail.com>
2022-11-01 15:59:33 +0530

    [rahul] Add:UC3-LengthComprasion

--- LineComprision/Program.cs
+++ LineComprision/Program.cs
@@ -9,8 +9,8 @@ namespace LineComprision
             Comprasion comprasion = new Comprasion(1,2,3,4);
             double line1 = comprasion.CalculateLength();
             Console.WriteLine("Length of first line " + line1);
-            Comprasion LineComparisionProblem = new Comprasion(1, 2, 3, 4);
-            double line2 = LineComparisionProblem.CalculateLength();
+            Comprasion LineComparisonProblem = new Comprasion(1, 2, 3, 4);
+            double line2 = LineComparisonProblem.CalculateLength();
             Console.WriteLine("Length of second line" + line2);
             if (line1 == line2)
             {
@@ -18,6 +18,13 @@ namespace LineComprision
             }
             else
                 Console.WriteLine("Not equal");
+            if (line1.CompareTo(line2) == 0)
+                Console.WriteLine("Both lines are same");
+            if (line1.CompareTo(line2) > 0)
+                Console.WriteLine("Line 1 is greater than line 2");
+            else
+                Console.WriteLine("Line 2 is greater than line 1");
+
         }
     }
 }
